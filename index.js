89349f109cc26d8f0406c5333a98aa20282d107c
Omar Shehata <oshehata862@gmail.com>
2025-04-26 00:45:55 +0300

    implement template replacement for product cards in Node Farm application

--- index.js
+++ index.js
@@ -2,8 +2,28 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-const data=fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8') 
+const templeteOverview=fs.readFileSync(`${__dirname}/templetes/overview.html`, 'utf-8');
+const templeteProduct=fs.readFileSync(`${__dirname}/templetes/product.html`, 'utf-8');
+const templeteCard=fs.readFileSync(`${__dirname}/templetes/card.html`, 'utf-8');
+
+
+const data=fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+
+ const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+    return output;
+}
+
 
 // Create an HTTP server
 const server = http.createServer((req, res) => {
@@ -12,12 +32,14 @@ const server = http.createServer((req, res) => {
     // overview page
     if(pathName === '/' || pathName === '/overview') {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>Welcome to the Overview Page</h1>');
+        
+        const cardsHtml = dataObj.map(el => replaceTemplate(templeteCard, el)).join('');
+        const output = templeteOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+        res.end(output);
 
     // API page
     }else if(pathName === '/api'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(data);
+        
 
     // Product page
     } else if(pathName === '/product') {
